b4977a915b330ef655f44d61f9903b0f732fc09f
An-kitB <ankit.bansal@bonamisoftware.com>
2022-07-01 16:15:37 +0530

    covert code into async-await instead of promises

--- app.js
+++ app.js
@@ -19,6 +19,74 @@ search.addEventListener('click', (event) => {
 });
 
 
+
+
+
+async function getWeather(city){
+    try{
+        var urlRes = await fetch(`${apiInfo.baseUrl}?q=${city}&appid=${apiInfo.key}&units=metric`)
+        //console.log(ur.json())
+        //var target = ur.json()
+
+        //let response = await fetch(targtetUrl);
+        let target = await urlRes.json();
+        console.log(target)
+        showWeather(target)
+        
+    }catch(err){
+        console.log(`Your Error : ${err}`)
+        //let target = await urlRes.json();
+        errorFun(target)
+    }
+
+}
+
+function errorFun(target){
+
+    let Err = document.getElementById('Error');
+    Err.innerHTML = `Code :${target.cod} & Msg : ${target.message}`;
+
+
+}
+
+function showWeather(target){
+    console.log("working")
+    
+    let city = document.getElementById('city');
+    city.innerText = `${target.name}, ${target.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `TEMP  :${Math.round(target.main.temp)} C or ${c2f(target.main.temp)} F`;
+
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `Status: ${target.weather[0].main}`;
+
+    let humid = document.getElementById('humidity');
+    humid.innerText = `HUMIDITY : ${target.main.humidity}`;
+    
+
+}
+
+
+
+
+
+
+
+
+function c2f(temp){
+
+    let tempFh = (temp * 9/5) + 32
+    return tempFh 
+
+
+}
+
+
+//Promises
+/*
+
 function getWeather(city) {
     fetch(`${apiInfo.baseUrl}?q=${city}&appid=${apiInfo.key}&units=metric`)
     .then(target => {
@@ -57,12 +125,5 @@ function showWeather(target){
     
 
 }
-function c2f(temp){
-
-    tempFh = (temp * 9/5) + 32
-    return tempFh 
-
-
-}
-
+*/
 
